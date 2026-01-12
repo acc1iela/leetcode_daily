6251396c36f227c2df2aca842c96c38d640812a8
acc1iela <acc1iela@gmail.com>
2026-01-12 12:10:11 +0900

    add other solution

---
+++ search_insert_position/other_solution.ts
@@ -0,0 +1,18 @@
+function searchInsertOther(nums: number[], target: number): number {
+  let left = 0;
+  let right = nums.length;
+
+  while (left < right) {
+    const mid = left + Math.floor((right - left) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    } else if (target > nums[mid]) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+
+  return left;
+}
