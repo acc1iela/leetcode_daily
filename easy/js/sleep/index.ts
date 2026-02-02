40c7102a323b44747aa7783eefa45243d8437039
acc1iela <acc1iela@gmail.com>
2026-02-03 00:11:50 +0900

    update my solution

--- easy/js/sleep/index.ts
+++ easy/js/sleep/index.ts
@@ -1,3 +1,5 @@
 async function sleep(millis: number): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, millis));
 }
+// Runtime 37ms | Beats 91.58%
+// Memory 55.48MB | Beats 22.79%
