057dca178ba7ed7ab8f59ca83afff2a2b76f1fd1
acc1iela <acc1iela@gmail.com>
2026-02-10 20:25:48 +0900

    add my solution

---
+++ easy/linked_list_cycle/index.ts
@@ -0,0 +1,29 @@
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function hasCycle(head: ListNode | null): boolean {
+  if (!head) return false;
+
+  let slow: ListNode | null = head;
+  let fast: ListNode | null = head;
+
+  while (fast !== null && fast.next !== null) {
+    slow = slow!.next;
+    fast = fast.next.next;
+
+    if (slow === fast) return true;
+  }
+
+  return false;
+}
+
+/**
+ * runtime 50ms | beats 48.94%
+ * Memory 58.03MB | Beats 68.48%
+ */
