a22770a4284868a77e3812145a2504b378380317
acc1iela <acc1iela@gmail.com>
2026-02-18 22:55:00 +0900

    add other solution

---
+++ easy/remove_duplicates_sorted_array/other_solution.ts
@@ -0,0 +1,12 @@
+const removeDuplicatesOther = (nums: number[]): number => {
+  let k: number = 1;
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] !== nums[i - 1]) {
+      nums[k] = nums[i];
+      k++;
+    }
+  }
+
+  return k;
+};
