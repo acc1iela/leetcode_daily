03c70ade2b4a7aa236cabd2fa0062d63f1225bfa
acc1iela <acc1iela@gmail.com>
2026-02-27 01:43:01 +0900

    add my solution

---
+++ easy/balanced_binary_tree/index.ts
@@ -0,0 +1,28 @@
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+function isBalanced(root: TreeNode | null): boolean {
+  if (root === null) return true;
+  return getHeight(root) !== -1;
+  function getHeight(node: TreeNode | null): number {
+    if (node === null) return 0;
+    const leftHeight = getHeight(node.left);
+    const rightHeight = getHeight(node.right);
+    if (leftHeight === -1 || rightHeight === -1 || Math.abs(leftHeight - rightHeight) > 1)
+      return -1;
+    return Math.max(leftHeight, rightHeight) + 1;
+  }
+}
+
+/**
+ * runtime 1ms | Beats 83%
+ * memory 61.16MB | Beats 41.94%
+ */
